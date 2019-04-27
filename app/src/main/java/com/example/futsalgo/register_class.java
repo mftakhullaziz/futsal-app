c451acd9f6901c0ed6a5628f2708c62ea97e9e66
MiftakhulAziz <azizmiftakhul19@gmail.com>
2019-04-28 03:37:41 +0700

    upadate back-end user register

--- app/src/main/java/com/example/futsalgo/register_class.java
+++ app/src/main/java/com/example/futsalgo/register_class.java
@@ -13,41 +13,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.EditText;
-import android.graphics.Typeface;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
+import com.airbnb.lottie.LottieAnimationView;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import com.airbnb.lottie.LottieAnimationView;
-import com.google.firebase.database.core.Context;
 
 public class register_class extends AppCompatActivity {
+
+    private EditText inputNama,
+            inputEmail,
+            inputPassword,
+            inputNoTelepon;
+
+    private String idPengguna,
+            nama,
+            email,
+            password,
+            telepon;
+
+    private FirebaseAuth auth;
+    private Button btnSignUp;
+    private ProgressDialog mProgress;
+    private DatabaseReference dbRef;
 
     ImageView imgUserPhoto;
     static int PReqCode = 1;
@@ -58,6 +65,67 @@ public class register_class extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        //Start Inisialisasi Firebase
+
+        auth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
+        mProgress = new ProgressDialog(this);
+
+        inputNama = (EditText) findViewById(R.id.nama_daftar);
+        inputEmail = (EditText) findViewById(R.id.email_daftar);
+        inputPassword = (EditText) findViewById(R.id.password_daftar);
+        inputNoTelepon = (EditText) findViewById(R.id.no_hp);
+        btnSignUp = (Button) findViewById(R.id.button_Daftar);
+
+        //End Inisialisasi Firebase
+
+        //Start Button Sign Up
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mProgress.setMessage("Mendaftar ...");
+                email = inputEmail.getText().toString().trim();
+                password = inputPassword.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    inputEmail.setError("Wajib diisi");
+                }
+                if (password.isEmpty()) {
+                    inputPassword.setError("Wajib diisi");
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password terlalu pendek, masukkan minimal 6 karakter!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mProgress.show();
+
+                //create user
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(register_class.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            showDialog();
+                            mProgress.dismiss();
+                        } else {
+                            startActivity(new Intent(register_class.this, login_class.class));
+                            simpanData();
+                            Toast.makeText(register_class.this, "Daftar Berhasil", Toast.LENGTH_SHORT).show();
+                            mProgress.dismiss();
+                            finish();
+                        }
+                    }
+                });
+            }
+        });
+
+        //End Button Sign Up
+
+
         Intent intent = getIntent();
 
         imgUserPhoto = findViewById(R.id.userPhoto);
@@ -71,6 +139,41 @@ public class register_class extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    private void simpanData() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        idPengguna = user.getUid();
+
+        nama = inputNama.getText().toString().trim();
+        email = inputEmail.getText().toString().trim();
+        password = inputPassword.getText().toString().trim();
+        telepon = inputNoTelepon.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mProgress.show();
+            dbRef.child("pemesan").child(idPengguna).child("idPemesan").setValue(idPengguna);
+            dbRef.child("pemesan").child(idPengguna).child("nama").setValue(nama);
+            dbRef.child("pemesan").child(idPengguna).child("email").setValue(email);
+            dbRef.child("pemesan").child(idPengguna).child("telepon").setValue(telepon);
+
+        }
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Pemberitahuan")
+                .setCancelable(true)
+                .setMessage("Daftar Gagal")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+        alert.show();
     }
 
     private void checkAndRequestForPermission() {
