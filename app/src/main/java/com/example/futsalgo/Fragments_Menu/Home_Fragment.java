53e30e762247521e1961ea14d39c67418d4d9a48
MiftakhulAziz <azizmiftakhul19@gmail.com>
2019-05-10 16:19:34 +0700

    upadate back-end Apps

--- app/src/main/java/com/example/futsalgo/Fragments_Menu/Home_Fragment.java
+++ app/src/main/java/com/example/futsalgo/Fragments_Menu/Home_Fragment.java
@@ -1,48 +1,36 @@
 package com.example.futsalgo.Fragments_Menu;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
+import com.example.futsalgo.MenuBeranda.ViewImageActivity;
 import com.example.futsalgo.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Home_Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Home_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Home_Fragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     private OnFragmentInteractionListener mListener;
 
+
+    GridView androidGridView;
+    Context context;
     public Home_Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Home_Fragment newInstance(String param1, String param2) {
         Home_Fragment fragment = new Home_Fragment();
         Bundle args = new Bundle();
@@ -64,11 +52,68 @@ public class Home_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootV = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        CardView BookingLapangan=rootV.findViewById(R.id.card1);
+        BookingLapangan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent start1 = new Intent(getActivity(), ViewImageActivity.class);
+                startActivity(start1);
+            }
+        });
+
+        CardView StatusBooking=rootV.findViewById(R.id.card2);
+        StatusBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent start1 = new Intent(getActivity(), BerandaActivity.class);
+                startActivity(start1);*/
+            }
+        });
+
+        CardView Location=rootV.findViewById(R.id.card3);
+        Location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent start1 = new Intent(getActivity(), BerandaActivity.class);
+                startActivity(start1);*/
+            }
+        });
+
+        CardView Reviews=rootV.findViewById(R.id.card4);
+        Reviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent start1 = new Intent(getActivity(), BerandaActivity.class);
+                startActivity(start1);*/
+            }
+        });
+
+        CardView kalenders=rootV.findViewById(R.id.card5);
+        kalenders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent start1 = new Intent(getActivity(), BerandaActivity.class);
+                startActivity(start1);*/
+            }
+        });
+
+        CardView About=rootV.findViewById(R.id.card6);
+        About.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*Intent start1 = new Intent(getActivity(), BerandaActivity.class);
+                startActivity(start1);*/
+            }
+        });
+
+        return rootV;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -92,18 +137,7 @@ public class Home_Fragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/example/futsalgo/Fragments_Menu/Profile_Fragment.java
+++ app/src/main/java/com/example/futsalgo/Fragments_Menu/Profile_Fragment.java
@@ -1,32 +1,53 @@
 package com.example.futsalgo.Fragments_Menu;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.futsalgo.R;
+import com.example.futsalgo.Activities.UbahProfilActivity;
+import com.example.futsalgo.Model.Pemesan;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Profile_Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Profile_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Profile_Fragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    private DatabaseReference dbRef;
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private String IDPengguna, name, email, noTelepon;
+    String PersonName , PersonEmail, PersonAlamat, PersonTelepon;
+
+    private String idPemesan;
+
+    private TextView textViewNama, textViewEmail, textViewNoTelepon, textViewAlamat;
+
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Context context;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,15 +55,7 @@ public class Profile_Fragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Profile_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static Profile_Fragment newInstance(String param1, String param2) {
         Profile_Fragment fragment = new Profile_Fragment();
         Bundle args = new Bundle();
@@ -55,20 +68,87 @@ public class Profile_Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        context = rootView.getContext();
+
+        textViewNama = (TextView) rootView.findViewById(R.id.tvNama);
+        textViewEmail = (TextView) rootView.findViewById(R.id.tvEmail);
+        textViewAlamat = (TextView) rootView.findViewById(R.id.tvAlamat);
+        textViewNoTelepon = (TextView) rootView.findViewById(R.id.tvNotelp);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference("pengguna");
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        assert user != null;
+        IDPengguna = user.getUid();
+
+        /*auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        assert user != null;
+
+        idPengguna = user.getUid();
+        PersonName = user.getDisplayName();
+        PersonEmail = user.getEmail();
+        PersonTelepon = user.getPhoneNumber();
+
+        TextView nav_user = (TextView)rootView.findViewById(R.id.tvNama);
+        TextView nav_email = (TextView)rootView.findViewById(R.id.tvEmail);
+        TextView nav_telepon = (TextView)rootView.findViewById(R.id.tvNotelp);
+        ImageView nav_photo = (ImageView) rootView.findViewById(R.id.fotoProfil_user);
+        nav_user.setText(PersonName);
+        nav_email.setText(PersonEmail);
+        nav_telepon.setText(PersonTelepon);
+        Glide.with(this).load(user.getPhotoUrl()).into(nav_photo);*/
+
+        ImageView nav_photo = (ImageView) rootView.findViewById(R.id.fotoProfil_user);
+        Glide.with(this).load(user.getPhotoUrl()).into(nav_photo);
+
+        getDataPemesan();
+
+        Button UbahProfil = (Button) rootView.findViewById(R.id.btn_ubah);
+            UbahProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, UbahProfilActivity.class);
+                startActivity(intent);
+            }
+        });
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    public void getDataPemesan() {
+        dbRef.child(IDPengguna).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Pemesan dataPemesan = dataSnapshot.getValue(Pemesan.class);
+                if (dataPemesan != null) {
+                    textViewNama.setText(dataPemesan.getNama());
+                    textViewEmail.setText(dataPemesan.getEmail());
+                    textViewAlamat.setText(dataPemesan.getAlamat());
+                    textViewNoTelepon.setText(dataPemesan.getTelepon());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -92,16 +172,6 @@ public class Profile_Fragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
